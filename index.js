469008546ad518ac21b972403e37358b18373d62
Aida <edigeeva.aika@mail.ru>
2023-02-16 11:49:05 +0600

    calculated  cone volume function

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function myName() {
   console.log( "My name is " , "Aida " , "Edigeeve ");
 }
 myName();
+
+function coneVolume(h,r){
+  let volum = 3 / 1 * h * r ;
+  console.log(volum);
+}
+coneVolume(12,6);
