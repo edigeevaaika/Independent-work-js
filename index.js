c1ed468bb59e8f4d88fea6a9be13b12e9ae77b8a
Aida <edigeeva.aika@mail.ru>
2023-02-18 08:31:33 +0600

    changed the meaning of the decision

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ function myName() {
 myName();
 
 function coneVolume(h,r){
-  let volum = 3 / 1 * h * r ;
+  let volum = 1 / 3 * h * r ;
   console.log(volum);
 }
 coneVolume(12,6);
+
+function stringLength( a, b, c ){
+  return a * b * c;
+}
+console.log (stringLength(5,7,5));
+
