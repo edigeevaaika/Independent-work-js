9d0ac3051f049502828b0de1c4df1bff47c154dd
Aida <edigeeva.aika@mail.ru>
2023-02-19 17:55:55 +0600

    wrote a function for finding the perimeter of a 5-gon

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 function myName() {
-  console.log( "My name is " , "Aida " , "Edigeeve ");
+  console.log("My name is ", "Aida ", "Edigeeve ");
 }
 myName();
 
-function coneVolume(h,r){
-  let volum = 1 / 3 * h * r ;
+function coneVolume(h, r) {
+  let volum = (1 / 3) * h * r;
   console.log(volum);
 }
-coneVolume(12,6);
-
+coneVolume(12, 6);
 
 function getStringLength(str) {
   return str.length;
 }
-let myString = 'Привет, мир!';
+let myString = "Привет, мир!";
 let stringLength = getStringLength(myString);
 console.log(stringLength); // выведет 13
-
 
 function number(a) {
   if (a % 2 === 0) {
@@ -27,8 +25,18 @@ function number(a) {
 }
 number(43);
 
-
-function squareNumber(a){
+function squareNumber(a) {
   return a * a;
 }
-console.log(squareNumber(4));
+console.log(squareNumber(9));
+
+
+
+// Math.sqrt(25);
+
+function perimeter(a,b,c,d,e){
+  return a + b + c + d + e
+}
+console.log(perimeter(2,4,7,37,89));
+
+
