c20229ca54f9a37ec78b61e9cf8d430418ec6108
Aida <edigeeva.aika@mail.ru>
2023-02-20 18:07:16 +0600

    I wrote a loop that prints odd numbers from 1 to 15.

--- index.js
+++ index.js
@@ -34,26 +34,26 @@ console.log(squareNumber(9));
 
 // Math.sqrt(25);
 function sqrtNumber(a){
-  return Math.sqrt(a)
+  return Math.sqrt(a);
 }
-console.log(sqrtNumber(25))
+console.log(sqrtNumber(25));
 
 function perimeter(a,b,c,d,e){
-  return a + b + c + d + e
+  return a + b + c + d + e;
 }
 console.log(perimeter(2,4,7,37,89));
 
 for (let i = 0; i < 34; i = i + 3){
-  console.log(i)
+  console.log(i);
 }
 
 //9. Напишите цикл, вычисляющий факториал числа 8. (8!)
 // ...
-let factorialResult = 1
+let factorialResult = 1;
 for (let i = 1; i < 9 ; i++ ){
-  factorialResult = factorialResult * i
+  factorialResult = factorialResult * i;
 }
-console.log(factorialResult)
+console.log(factorialResult);
 
 //10. Напишите цикл, который выводит квадраты чисел от 1 до 10.
 // 1 4 9 ...
@@ -81,26 +81,31 @@ for (let i = 0; i < 21; i ++){
 //13. Напишите цикл, который печатает нечетные числа от 1 до 15.
 // 1 3 ...
 
+for ( let i = 1; i < 16; i ++){
+  if (i % 2 !== 0){
+    console.log (i);
+  }
+}
 //14. Создайте массив из строк и добавьте в начало массива 2 новых элемента, а в конец 3.
 
 //15. Создайте массив чисел и вычислите сумму всех четных чисел в массиве.
-let number3 = [2,5,8,12,5];
-let sum2 = 0
-for (let i = 0; i < number3.length; i ++){
-if (number3[i] % 2 === 0) {
-  sum2 = sum2 + number3[i];
-}
-}
-console.log(sum2);
+// let number3 = [2,5,8,12,5];
+// let sum2 = 0
+// for (let i = 0; i < number3.length; i ++){
+// if (number3[i] % 2 === 0) {
+//   sum2 = sum2 + number3[i];
+// }
+// }
+// console.log(sum2);
 
 //21. Создайте массив строк и проверьте есть ли такой элемент в массиве. // ["cat", "dog", "snake"] 
 // cat - true
 // parrot - false
- let example = ["parrot","horse","cat", "dog", "snake","pig",];
- for (let i = 0; i < example.length; i ++){
-  if (example[i] === "cat"||example[i] === "dog"||example[i] == "snake"){
-    console.log (example[i] + " - true" );
-  } else {
-    console.log (example[i] + " - false" );
-  }
- }
+//  let example = ["parrot","horse","cat", "dog", "snake","pig",];
+//  for (let i = 0; i < example.length; i ++){
+//   if (example[i] === "cat"||example[i] === "dog"||example[i] == "snake"){
+//     console.log (example[i] + " - true" );
+//   } else {
+//     console.log (example[i] + " - false" );
+//   }
+//  }
